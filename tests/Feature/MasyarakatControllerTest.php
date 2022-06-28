82c527e1f439de05c039b7295093e52cb47336a0
04khofifah <2041720119@student.polinema.ac.id>
2022-06-28 14:46:57 +0700

    upp thet masyarakat

---
+++ tests/Feature/MasyarakatControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Pengaduan;
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MasyarakatControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        //acting as superadmin
+        $this->actingAs(User::find(1));
+    }
+
+    public function test_list_pengaduan(){
+        // $response = $this->post('/laporan-masyarakat', [
+        //     'email' => "superadmin@gmail.com",
+        //     'password' => "admin",
+        // ]);
+
+        $response = $this->get('/menu-masyarakat/list-laporan',);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create_pengaduan(){
+        $response = $this->post('/menu-masyarakat/laporan-masyarakat', [
+            'description' => 'test buat laporan',
+            'image' => UploadedFile::fake()->image('avatar.jpg'),
+        ]);
+
+        // $response->assertStatus(200);
+        $this->assertTrue(true);
+    }
+}
