a25df0a085d74718f69703056725d5eeb8b36c71
= <=>
2022-06-28 13:01:16 +0700

    pembuatan halaman welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,3 +69,99 @@
 		</div>
 	</section>
 	<!-- Hero section end -->
+
+    <!-- About section -->
+	<section class="about-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 offset-lg-6 about-text">
+					<h2>Aduin</h2>
+					{{-- <h5></h5> --}}
+                    <br>
+					<p>Pengaduan masyarakat adalah penyampaian keluhan oleh masyarakat kepada pemerintah
+                         atas pelayanan yang tidak sesuai dengan standar pelayanan, atau pengabaian kewajiban dan/atau pelanggaran larangan.</p>
+                        <br>
+                        <br>
+					<a href="{{ route('login') }}" class="site-btn sb-gradients sbg-line mt-5">Get Started</a>
+				</div>
+			</div>
+			<div class="about-img">
+				<img src="{{ asset('homepage/img/hero.svg')}}" alt="">
+			</div>
+		</div>
+	</section>
+	<!-- About section end -->
+
+
+	<!-- Features section -->
+	<section class="features-section spad gradient-bg">
+		<div class="container text-white">
+			<div class="section-title text-center">
+				<h2>Alur Pengaduan</h2>
+			</div>
+			<div class="row">
+				<!-- feature -->
+				<div class="col-md-3 feature">
+					<i class="ti-pencil"></i>
+					<div class="feature-content">
+						<h4>Tulis Laporan</h4>
+						<p>Tulis laporan keluhan Anda dengan jelas.</p>
+						{{-- <a href="" class="readmore">Readmore</a> --}}
+					</div>
+				</div>
+				<!-- feature -->
+				<div class="col-md-3 feature">
+					<i class="ti-reload"></i>
+					<div class="feature-content">
+						<h4>Proses Verifikasi</h4>
+						<p>Tunggu sampai laporan Anda di verifikasi.</p>
+						{{-- <a href="" class="readmore">Readmore</a> --}}
+					</div>
+				</div>
+				<!-- feature -->
+				<div class="col-md-3 feature">
+					<i class="ti-settings"></i>
+					<div class="feature-content">
+						<h4>Tindak Lanjut</h4>
+						<p>Laporan Anda sedang dalam tindak lanjut.</p>
+						{{-- <a href="" class="readmore">Readmore</a> --}}
+					</div>
+				</div>
+				<!-- feature -->
+				<div class="col-md-3 feature">
+					<i class="ti-check-box"></i>
+					<div class="feature-content">
+						<h4>Selesai</h4>
+						<p>Laporan pengaduan telah selesai ditindak.</p>
+						{{-- <a href="" class="readmore">Readmore</a> --}}
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Features section end -->
+
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<div class="container">
+			<div class="row spad">
+				<div class="col-md-6 col-lg-3 footer-widget pl-lg-5 pl-3">
+					<h5 class="widget-title"> Kelompok 5 - UAS PWL </h5>
+					<div class="social">
+                        <p>Khofifah Amanda [2041720119]</p>
+                        <p> Safira Dyah Satiti [2041720094]</p>
+                        <p> Siti Zumaroh D.F [2041720058]</p>
+					</div>
+				</div>
+			</div>
+	</footer>
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="{{ asset('homepage/js/jquery-3.2.1.min.js') }}"></script>
+	<script src="{{ asset('homepage/js/owl.carousel.min.js')}}"></script>
+	<script src="{{ asset('homepage/js/main.js')}}"></script>
+</body>
+
+</html>
